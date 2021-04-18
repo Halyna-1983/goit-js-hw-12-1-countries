5811e13083189dad910d89bd56492d742f91542c
Halyna-1983 <72556196+Halyna-1983@users.noreply.github.com>
2021-04-18 14:19:56 +0300

    mistake was founded

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { debounce } from 'lodash';
 import API from './js/fetchCountries';
-import countriesListTemplate from './templates/countriesList.hbs'
-import countryCardTemplate from './templates/countriesCard.hbs'
+// import countriesListTemplate from './templates/countriesList.hbs'
+// import countryCardTemplate from './templates/countriesCard.hbs'
 // import { error } from '@pnotify/core';
 // import '@pnotify/core/dist/BrightTheme.css';
 // import '@pnotify/core/dist/PNotify.css';
@@ -9,43 +9,23 @@ import countryCardTemplate from './templates/countriesCard.hbs'
 
 const refs = {
   searchForm: document.querySelector('.js-search-form'),
-  output: document.querySelector('.container'),
+  output: document.querySelector('container'),
 };
 
 refs.searchForm.addEventListener('input', debounce(onSearch, 500))
 
 function onSearch(event) {
-  refs.output.innerHTML = '';
-   
-  const searchQuery = event.target.value.trim();
-  if (searchQuery === "") {
-    return
-  }
-  
-  API.fetchCountriesList(searchQuery).then(data => {
-    if (!data || !data.length) {
-  return
-  };
-    if (data.length > 10) {
-      console.warn("Too many items");
-      error({
-        text: 'Too many items to show, please enter more specific query',
-        delay: 4000,
-      });
-    } else if (data.length <= 10 && data.length > 1) {
-            
-      renderCountriesList(data);
-     }
-      else if (data.length === 1) {
-      renderCountryCard(data)
-     }
-})
+    const searchQuery = event.target.value;
+     
+  API.fetchCountries(searchQuery)
 }
 
-function renderCountriesList(listData) {
-  refs.output.innerHTML = countriesListTemplate(listData)
-}
+console.log(API.BASE_URL);
 
-function renderCountryCard(countryData) {
-  refs.output.innerHTML = countryCardTemplate(countryData)
-}
+// function renderCountriesList(listData) {
+//   refs.output.innerHTML = countriesListTemplate(listData)
+// }
+
+// function renderCountryCard(countryData) {
+//   refs.output.innerHTML = countryCardTemplate(countryData)
+// }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,10 @@
-const BASE_URL = 'https://restcountries.eu/rest/v2/'
+
+const BASE_URL = 'https://restcountries.eu/rest/v2/';
 
 function fetchCountries(searchQuery) {
-    fetch (`${BASE_URL}/name/${searchQuery}`)
+    fetch(`${BASE_URL}name/${searchQuery}`)
     .then(result => result.json())
     .then(console.log)
 }
-export default {fetchCountries}
+
+export default { fetchCountries };
